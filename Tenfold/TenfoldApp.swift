fb73f1eccaad34f8df460713bdd48645ed9a9f2e
Deborah Kalanit <dbkalanit@gmail.com>
2026-03-08 16:29:09 -0700

    Initial Commit

---
+++ Tenfold/TenfoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenfoldApp.swift
+//  Tenfold
+//
+//  Created by Deborah Brown on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TenfoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
