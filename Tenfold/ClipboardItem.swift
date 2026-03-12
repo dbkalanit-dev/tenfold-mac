8ec8824cf3625fd548ca32bd313bdf8fe4ce94ed
Deborah Kalanit <dbkalanit@gmail.com>
2026-03-11 21:43:48 -0700

    Initial commit: Tenfold MVP with unified project structure

--- Tenfold/ClipboardItem.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct ClipboardItem: Identifiable {
-    let id: Int // 1-10
-    var content: String
-    var timestamp: Date
-    
-    // We'll add more properties here later (like data type or masking)
-}
